49b1bc65481ff6f67af680fe8309bd0ec3b65a1a
pi user <leonardo.mogianesi@studenti.unicam.it>
2022-05-03 15:40:59 +0200

    Imported jspdf and modified some graphics

--- front-end-angular/gestionale-lavanderia/src/app/pages/fatture/fatture.component.ts
+++ front-end-angular/gestionale-lavanderia/src/app/pages/fatture/fatture.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-fatture',
@@ -9,6 +10,18 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class FattureComponent implements OnInit {
 
   constructor() { }
+
+  doc = new jsPDF();
+
+  createPDF(){
+  // Default export is a4 paper, portrait, using millimeters for units
+  this.doc.text("Hello world!", 10, 10);
+  this.doc.save("a4.pdf");
+  }
+
+  importPDF(){
+  }
+
 
   ngOnInit(): void {
   }
