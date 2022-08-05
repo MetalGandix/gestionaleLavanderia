15effbd91462bd0e0571675fee8bd49368b5bd55
pi user <leonardo.mogianesi@studenti.unicam.it>
2022-08-05 18:00:03 +0200

    Added findByEmail

--- magistralThesis/src/main/java/gestionaleLavanderia/magistralThesis/repository/UserDaoRepository.java
+++ magistralThesis/src/main/java/gestionaleLavanderia/magistralThesis/repository/UserDaoRepository.java
@@ -11,6 +11,8 @@ import gestionaleLavanderia.magistralThesis.model.DAOUser;
 public interface UserDaoRepository extends JpaRepository<DAOUser, Long> {
 	DAOUser findByUsername(String username);
 
+	DAOUser findByEmail(String email);
+
 	@Query("SELECT u FROM DAOUser u WHERE u.username LIKE %?1%")
 	List<DAOUser> searchUser(String username);
 }
