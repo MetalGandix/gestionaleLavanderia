642052f1dec98c94d4774eeeb9b534da39b8c1f2
pi user <leonardo.mogianesi@studenti.unicam.it>
2022-08-05 17:28:42 +0200

    Removed user infos

--- front-end-angular/gestionale-lavanderia/src/app/classes/user.ts
+++ front-end-angular/gestionale-lavanderia/src/app/classes/user.ts
@@ -6,14 +6,5 @@ export class User {
 	number: string;
 	email: string;
 	sesso: string;
-	codiceFiscale: string;
-	presentatoDa: string;
-	numeroCard: string;
-	scontoCard: string;
-	cap: string;
-	citta: string;
-	regioneSociale: string;
 	indirizzo: string;
-	provincia: string;
-	dataConsegna: Date;
 }
